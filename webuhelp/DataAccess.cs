757a91b184a4041975be3c55d8ae00bceb5e36fe
Kaya Kopp <k.kopp@neogeo.com>
2023-03-08 09:04:48 +0100

    wip befehl-s

---
+++ webuhelp/DataAccess.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Data.SQLite;
+
+namespace webuhelp
+{
+    internal class DataAccess
+    {
+        private static SQLiteConnection GetOpenConnection()
+        {
+            var connection = new SQLiteConnection($"Data Source=WebUntisDB.db;Version=3;");
+            connection.Open();
+            return connection;
+        }
+
+        public List<PupilRow> GetSummaryData()
+        {
+            // Status = entsch., nicht entsch. Grund = Verspätet Entschuldigungstext = B
+            var query = @"SELECT
+                            P.schuelerID,
+                            P.klasse,
+                            P.name,
+                            P.vorname,
+                            (SELECT SUM(*) 
+                                FROM Absence A 
+                                WHERE A.schuelerID = P.schuelerID
+                                AND status = 'entsch.';";
+        }
+
+    }
+}
---
+++ webuhelp/PupilRow.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace webuhelp
+{
+    public class PupilRow
+    {
+        public string Name { get; set; }
+        public string  FirstName { get; set; }
+        public int A { get; set; }
+        public int N { get; set; }
+        public int B { get; set; }
+        public int V { get; set; }
+    }
+}
