370fbdf2faa54171001efacacb4548385f9cf6aa
Kaya Kopp <k.kopp@neogeo.com>
2023-03-10 10:57:56 +0100

    adds complete query DBNull helper method and data access

--- webuhelp/DataAccess.cs
+++ webuhelp/DataAccess.cs
@@ -12,19 +12,71 @@ namespace webuhelp
             return connection;
         }
 
-        public List<PupilRow> GetSummaryData()
+        private static int? GetIntNullable(SQLiteDataReader reader, int index)
         {
-            // Status = entsch., nicht entsch. Grund = Verspätet Entschuldigungstext = B
-            var query = @"SELECT
-                            P.schuelerID,
-                            P.klasse,
-                            P.name,
-                            P.vorname,
-                            (SELECT SUM(*) 
-                                FROM Absence A 
-                                WHERE A.schuelerID = P.schuelerID
-                                AND status = 'entsch.';";
+            if (reader.IsDBNull(index))
+            {
+                return null;
+            }
+            return reader.GetInt32(index);
         }
 
+        public List<PupilRow> GetSummaryData()
+        {
+            var query = @"SELECT
+                            P.name,
+                            P.vorname,
+                            (
+                                SELECT
+                                COUNT(*) 
+                                WHERE A.status = 'entsch.'
+                             ) AS excused,
+                             (
+                                SELECT
+                                COUNT(*)
+                                WHERE A.status = 'nicht entsch.'
+                             ) AS not_excused,
+                             (
+                                SELECT
+                                COUNT(*)
+                                WHERE A.entschuldigungstext = 'B'
+                             ) AS work_related,
+                             (
+                                SELECT
+                                COUNT(*)
+                                WHERE A.grund = 'Verspätet'
+                             ) AS late
+                            FROM Pupil P
+                            JOIN Absence A ON A.schuelerID = P.schuelerID                                
+                            ;";
+
+            var rowList = new List<PupilRow>();
+
+            using (var connection = GetOpenConnection())
+            {
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = query;
+                    
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var row = new PupilRow();
+
+                            row.Name = reader.GetString(0);
+                            row.FirstName= reader.GetString(1);
+                            row.A = GetIntNullable(reader, 2) ?? 0;
+                            row.N = GetIntNullable(reader, 3) ?? 0;
+                            row.B = GetIntNullable(reader, 4) ?? 0;
+                            row.V = GetIntNullable(reader, 5) ?? 0;
+
+                            rowList.Add(row);
+                        }
+                    }
+                }
+            }
+            return rowList;
+        }
     }
 }
